19140a1e1b7edb8636016ed50e166bf69e997a13
nekorobi_0 <65654766+nekorobi-0@users.noreply.github.com>
2020-08-01 05:53:27 +0900

    miss

--- 使用例.py
+++ 使用例.py
@@ -4,4 +4,4 @@ print(seichi_ranking.ranking())
 #週間建築ランキングの上位30人を表示そして単位を入れる
 print(seichi_ranking.ranking(duration="weekly",ranking_type="build",lim=30,unit=True))
 #普通にランキング
-print(seichi_ranking(duration="",lim=150))
+print(seichi_ranking.ranking(duration="",lim=150))
